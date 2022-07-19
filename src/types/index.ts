173329c5a732952105d49a37b4f1383c338f9fd5
TkachenokPavel <pasha.tkachenok@mail.ru>
2022-07-19 13:00:31 +0300

    docs(hw-37): add types folder and add components files

---
+++ src/types/index.ts
@@ -0,0 +1,21 @@
+export type BadgeLabel = 'area' | 'population';
+
+export interface ICountry {
+    name: string,
+    capital: string[],
+    population: number,
+    flag: string,
+    area: number,
+    region: string
+}
+
+export enum Color {
+    Primary = 'primary',
+    Secondary = 'secondary',
+    Success = 'success',
+    Danger = 'danger',
+    Warning = 'warning',
+    Info = 'info',
+    Light = 'light',
+    Dark = 'dark',
+}
